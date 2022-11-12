b5436d0aa4cb0c170c0391b47aac309ecee342b2
EricchiYukia <git@ericchi.moe>
2022-11-12 16:45:30 +0100

    Fatto un po' di JS

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function mail() {
-    
+    let inputmail = document.getElementById("loginform").value;
+    console.log(inputmail);
+    const mailaccettate = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
+    for (let i=0; i< mailaccettate.length; i++) {
+        if (inputmail === mailaccettate[i]) {
+            // Qui routine per sbloccare l'accesso
+            console.log('Login effettuato.');
+            console.log(i);
+            break;
+        }
+        i++;
+    }
 }
