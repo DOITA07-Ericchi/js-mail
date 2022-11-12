25fdad886c61b8dddc2544afaac205f970cb5159
EricchiYukia <git@ericchi.moe>
2022-11-12 17:02:34 +0100

    Quasi fatta la prima parte

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ function mail() {
     let inputmail = document.getElementById("loginform").value;
     console.log(inputmail);
     const mailaccettate = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
-    for (let i=0; i< mailaccettate.length; i++) {
-        if (inputmail === mailaccettate[i]) {
+    let arealogin = document.getElementById('arealogin');
+    let risultato = document.getElementById("resultbox");
+    for (let i = 0; i < mailaccettate.length; i++) {
+        if (inputmail == mailaccettate[i]) {
             // Qui routine per sbloccare l'accesso
-            console.log('Login effettuato.');
+            console.log('Accesso effettuato.');
             console.log(i);
+            risultato.innerHTML("Accesso effettuato.");
             break;
+        } else {
+            risultato.innerHTML("Accesso negato.")
         }
-        i++;
+        arealogin.classList.remove("d-none");
+        arealogin.classList.add("d-block");
     }
 }
